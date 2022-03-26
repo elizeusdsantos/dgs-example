a3dd9d215a1f6b4fab99c29bb3e514355ea7dac3
Elizeu Santos <elizeusdsantos@gmail.com>
2022-03-26 11:10:28 +0100

    Convert into a Kotlin project

--- src/main/java/com/example/dgsexample/model/Company.java
+++
@@ -1,4 +0,0 @@
-package com.example.dgsexample.model;
-
-public record Company(Long id, String companyName, String domain, Integer yearFounded) {
-}
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -2,7 +2,7 @@
 DROP TABLE IF EXISTS companies;
 
 CREATE TABLE companies (
-  id INT auto_increment PRIMARY KEY,
+  id SERIAL PRIMARY KEY,
   company_name VARCHAR(200),
   domain VARCHAR(200),
   year_founded INT
